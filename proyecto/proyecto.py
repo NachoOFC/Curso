6f22a47f46d18c7583f88b50ff9f1e3165ba9c83
nachoofc <ignacioperezolavarria@gmail.com>
2022-06-04 19:36:24 -0400

    cambios con rodrigo

--- proyecto/proyecto.py
+++ proyecto/proyecto.py
@@ -1,5 +1,14 @@
+import random
 print("aqui hare el curso de github")
 print("Hola miamor")
-
+lista=[]
+for a in range (5):
+    lista.append (random.randint(1,10))
+for a in lista:
+    print (a)
 print("Hola gente del wsp")
-print("Los amo <3") 
+print("Los amo <3")
+
+print ("hola mundo!")
+
+
