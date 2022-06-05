1b38bf188e01f3645508dd2c0ee9af4c1aece2b5
nachoofc <ignacioperezolavarria@gmail.com>
2022-06-04 20:17:25 -0400

    aaaaa

--- proyecto/proyecto.py
+++ proyecto/proyecto.py
@@ -1,16 +1,3 @@
-from os import system
-system ("cls")
-import random
 print("aqui hare el curso de github")
-print("Hola miamor")
-lista=[]
-for a in range (5):
-    lista.append (random.randint(1,10))
-for a in lista:
-    print (a)
-print("Hola gente del wsp")
-print("Los amo <3")
-
-print ("hola mundo!")
-
-
+#¿donde estan todos los cambios?
+print("a")
